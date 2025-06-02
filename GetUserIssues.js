6b4e3834844ab0052b4ea35227b5e9b2661db731
Christophe Shumbusho <christopheshumbusho69@gmail.com>
2025-06-02 15:15:29 +0200

    Credit card issuer checking kata 1 challenge

---
+++ GetUserIssues.js
@@ -0,0 +1,25 @@
+function getIssuer(number) {
+  const numString = number.toString();
+  const length = numString.length;
+  
+  if(numString.startsWith("34") || numString.startsWith("37") && length === 15){
+    return "AMEX";
+  }
+  if(numString.startsWith("6011") && length === 16){
+    return "Discover";
+  }
+  if(numString.startsWith("51")||
+    numString.startsWith("52") ||
+    numString.startsWith("53") ||
+    numString.startsWith("54") ||
+    numString.startsWith("55") &&
+     length === 16
+    ){
+    return "Mastercard";
+  }
+  if(numString.startsWith("4") && (length === 13 || length === 16)){
+    return "VISA";
+  }
+  
+  return "Unknown";
+}
